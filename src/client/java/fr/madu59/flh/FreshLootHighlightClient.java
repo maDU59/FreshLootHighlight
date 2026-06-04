bf3ee7c82dc918c977bce8b9fe9fa94820b42a04
maDU59_ <matthias.ducourant@gmail.com>
2026-06-04 14:22:54 +0200

    Started making some changes

--- src/client/java/fr/madu59/flh/FreshLootHighlightClient.java
+++ src/client/java/fr/madu59/flh/FreshLootHighlightClient.java
@@ -19,6 +19,8 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import fr.madu59.flh.config.Option;
 import fr.madu59.flh.config.SettingsManager;
 import fr.madu59.flh.config.configscreen.FreshLootHighlightConfigScreen;
+import fr.madu59.flh.warnings.PickUpWarning;
+import fr.madu59.flh.warnings.PickUpWarningUtils;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
